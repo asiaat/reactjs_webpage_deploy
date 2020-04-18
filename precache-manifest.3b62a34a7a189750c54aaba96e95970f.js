4e7d07448b1466ea1164208936bf046dd25fdaaa
kalle olumets <olumetskalle@gmail.com>
2020-04-18 11:39:19 +0200

    Updates

---
+++ precache-manifest.3b62a34a7a189750c54aaba96e95970f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cfbde028b1c4e78410562507c09bbb5",
+    "url": "/asiaat/reactjs_webpage_deploy/index.html"
+  },
+  {
+    "revision": "ab0925c3178127c480ba",
+    "url": "/asiaat/reactjs_webpage_deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "110766e28facfa1963b9",
+    "url": "/asiaat/reactjs_webpage_deploy/static/js/2.f1342a1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/asiaat/reactjs_webpage_deploy/static/js/2.f1342a1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab0925c3178127c480ba",
+    "url": "/asiaat/reactjs_webpage_deploy/static/js/main.89c2ea2b.chunk.js"
+  },
+  {
+    "revision": "000c7a4aa15fa8eb859a",
+    "url": "/asiaat/reactjs_webpage_deploy/static/js/runtime-main.da0dea6b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/asiaat/reactjs_webpage_deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
